4fdf4492e225d381abb231bf0faa76925f401672
tirunkumar <94432817+tirunkumar@users.noreply.github.com>
2021-12-14 18:00:13 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("world!");
         System.out.println("hello!");
+        System.out.println("hi!");
     }
 }
